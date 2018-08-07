97e9939a88f92a70ef0a4bdcf55b8b24fc47dbf4
Kennedycmr <kennedycmr@gmail.com>
2018-08-07 07:31:43 -0400

    Updated Configure-GitEnv to anonymize some variables

--- Configure-GitEnv/Configure-GitEnv.psm1
+++ Configure-GitEnv/Configure-GitEnv.psm1
@@ -2,16 +2,16 @@
 ### change proxyserver, proxyport and gitinstallloc if your values are different.
 
 function Configure-GitEnv() {
-  # need to add to $PATH where the git binaries are
-  $GITInstallLoc = 'C:\Users\kenc150\AppData\Local\GitHubDesktop\app-1.3.2\resources\app\git\cmd'
-  $ENV:PATH = $ENV:PATH + ";" + $GITInstallLoc
+  # need to add to $PATH where the git binaries are. Ensure this is setup in your $ENV:PATH environment. 
+  # Path should be similar to: C:\Users\{UserName}\AppData\Local\GitHubDesktop\app-1.3.2\resources\app\git\cmd
 
   # need to setup proxy so that we can use git. 
   $user = read-host -prompt 'Username for proxy server'
   $password = Read-Host 'Password for proxy server' -AsSecureString
   $password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
-  $proxyserver = '172.24.32.221'
+  # I used a hosts file entry to store the IP for the proxy server as there was a problem with GIT and our proxy load balancer. 
+  $proxyserver = 'PROXYDIRECT'
   $proxyport = '8081'
 
   $ENV:http_proxy = "http://'${user}':'${password}'@${proxyserver}:${proxyport}"
