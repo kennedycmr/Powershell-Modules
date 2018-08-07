9a166e3ce53e25b6b632452a0e2f3db29f338337
Kennedycmr <kennedycmr@gmail.com>
2018-08-07 07:41:29 -0400

    Added standard verbiage in Configure-GitEnv

--- Configure-GitEnv/Configure-GitEnv.psm1
+++ Configure-GitEnv/Configure-GitEnv.psm1
@@ -1,7 +1,28 @@
-### script to set up git session on Windows computer. 
-### change proxyserver, proxyport and gitinstallloc if your values are different.
-
 function Configure-GitEnv() {
+  <#
+  .Synopsis
+     Configure proxy environment so GIT can work when behind a corporate firewall requiring a proxy.  
+
+  .DESCRIPTION
+     This script will configure proxy environment variables with the required username/passwords. 
+   
+  .EXAMPLE
+     Configure-GitEnv
+   
+  .NOTES
+     Tested on PowerShell 6.5+ 
+     This will store your credentials in clear text as variables. consider the security implications for your setup. 
+   
+     AUTHOR: Cameron Kennedy
+     LAST EDIT: 08/07/2018 : Updated script to have verbiage. 
+   
+  .INPUTS
+   
+  .LINK
+    https://github.com/kennedycmr/Powershell-Modules/tree/master/Configure-GitEnv
+
+  #>
+
   # need to add to $PATH where the git binaries are. Ensure this is setup in your $ENV:PATH environment. 
   # Path should be similar to: C:\Users\{UserName}\AppData\Local\GitHubDesktop\app-1.3.2\resources\app\git\cmd
 
@@ -19,3 +40,14 @@ function Configure-GitEnv() {
 
   write-host "Git environment setup."
 }
+
+
+
+
+
+
+
+
+
+
+
